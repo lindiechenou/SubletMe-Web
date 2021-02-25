f6355eceb3e70880791980a6cbb204cfcc957f53
JBoyd12 <cooljb.289@gmail.com>
2021-02-25 00:03:56 -0500

    more updates to profile page

--- SubletMe-Web/js/profile.js
+++ SubletMe-Web/js/profile.js
@@ -14,10 +14,3 @@ function editButton1() {
     name.readOnly = true;
   }
 }
-
-var myModal = document.getElementById("myModal");
-var myInput = document.getElementById("myInput");
-
-myModal.addEventListener("shown.bs.modal", function () {
-  myInput.focus();
-});
