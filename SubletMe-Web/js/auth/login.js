e232af4ea38eb36b0a80d1f3bdd2bb3a989f1818
Lindie Chenou <lindiechenou@gmail.com>
2021-03-27 02:31:46 -0400

    Error handling for the login page

--- SubletMe-Web/js/auth/login.js
+++ SubletMe-Web/js/auth/login.js
@@ -1,41 +1,58 @@
 localStorage.setItem("info", "");
-async function login() {
-  //login fields
-  var email = document.querySelector("#loginEmail").value;
-  var loginEmail = email.toLowerCase();
-  const params = {
-    email: loginEmail,
-    password: document.querySelector("#loginPassword").value,
-  };
-
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", "http://localhost:8000/api/rest-auth/login/", true);
-  xhr.setRequestHeader("Content-type", "application/json");
-  xhr.send(JSON.stringify(params));
-  xhr.onreadystatechange = function () {
-    if (this.status == 200) {
-      var token = JSON.parse(xhr.responseText);
-      // console.log(token.key);
-      profileInfo(token.key);
-    } else {
-      // document.getElementById("error-message").innerHTML = xhr.responseText;
-    }
-  };
+function login(){
+  var email = (document.querySelector("#loginEmail").value).toLowerCase();
+  var password = document.querySelector("#loginPassword").value;
+  if(email != "" && password != ""){
+      fetch('http://localhost:8000/api/rest-auth/login/',{
+        method: 'post',
+        headers: new Headers({
+          'Content-Type': 'application/json',
+        }),
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        })
+      }).then(response =>{
+        if (response.ok) return response.json();
+        return response.json().then(response => {
+          var text = Object.keys(response)[0]
+          throw new Error(response[text])
+        })
+      }).then(response =>{profileInfo(response.key)})
+      .catch((error) => {
+        console.log(error.message)
+        document.getElementById('toastBody').innerHTML = error.message;
+        var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+        var toastList = toastElList.map(function(toastEl){
+          return new bootstrap.Toast(toastEl)
+        });
+        toastList.forEach(toast => toast.show());
+      })
+  }
+  else{
+    document.getElementById('toastBody').innerHTML = "Please fill out all the fields";
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+    var toastList = toastElList.map(function(toastEl){
+      return new bootstrap.Toast(toastEl)
+    });
+    toastList.forEach(toast => toast.show());
+  }
 }
 
-function profileInfo(tok) {
-  let xhr1 = new XMLHttpRequest();
-  localStorage.setItem("Token", tok);
-  xhr1.open("GET", "http://localhost:8000/api/rest-auth/user/");
-  xhr1.setRequestHeader("Authorization", `Token ${tok}`);
-  xhr1.send();
-  xhr1.onreadystatechange = function () {
-    if (this.status == 200) {
-      console.log(xhr1.responseText);
-      localStorage.setItem("info", xhr1.responseText);
-      window.location.href = "../../html/index.html";
-    }
-  };
+function profileInfo(keyID){
+  localStorage.setItem("Token", keyID);
+  fetch('http://localhost:8000/api/rest-auth/user/',{
+    method: 'get',
+    headers:  new Headers({
+      'Authorization':  `Token ${keyID}`
+    })
+  }).then(response => {
+    return response.text();
+  }).then(parsed_result => {
+    console.log(parsed_result)
+    localStorage.setItem("info", parsed_result);
+    window.location.href = "../../html/index.html";
+  })
 }
 
 var emailBox = document.getElementById("loginEmail");
@@ -79,3 +96,5 @@ $(".toggle-password").click(function () {
     password.setAttribute("type", "password");
   }
 });
+
+
